3df72c456b5d6aff242eece0c61f3554f64515e7
rhysez <121772484+rhysez@users.noreply.github.com>
2023-09-11 16:46:27 +0100

    basic styling for header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 const Header = (props) => {
-    // header component
+    
+    const navBarButtons = [
+        'About',
+        'Projects',
+        'Contact'
+    ]
+
+    const navBarButtonsMapped = navBarButtons.map((button, index) => <li key={index}>{button}</li>)
+
     return (
         <>
-            
+            <div className='header-container'>
+                <div className='nav-bar'>
+                    <ul className='nav-list'>
+                        {navBarButtonsMapped}
+                    </ul>
+                </div>
+                <div className='header-main'>
+                    <h2 className='header-title'>Rhys Hodgson.</h2>
+                </div>
+            </div>
         </>
     )
 }
