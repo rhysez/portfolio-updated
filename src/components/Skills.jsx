112a6fef5aa2ec038e3c33feffc463c24feb2e46
rhysez <121772484+rhysez@users.noreply.github.com>
2023-10-02 16:07:08 +0100

    add onClick function for changing color on img elements in Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,56 +1,74 @@
+import { useState } from 'react'
+
 const Skills = (props) => {
-    
+
+  const [selectedSkill, setSelectedSkill] = useState('Technologies I work with...')
+  const [skillColor, setSkillColor] = useState('var(--background-light)')
+
   const technologies = [
     {
-      name: "html",
+      name: "HTML - Markup",
+      color: 'orange',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
     },
     {
-      name: "css",
+      name: "CSS - Styling",
+      color: 'dodgerblue',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
     },
     {
-      name: "javascript",
+      name: "JavaScript - Programming",
+      color: 'yellow',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
     },
     {
-      name: "react",
+      name: "React - Library",
+      color: 'dodgerblue',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     },
     {
-      name: "git",
+      name: "Git - Version control",
+      color: 'mediumseagreen',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
     },
     {
-      name: "jest",
+      name: "Jest - Testing",
+      color: 'violet',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
     },
     {
-      name: "webpack",
+      name: "Webpack - Bundler",
+      color: 'dodgerblue',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg",
     },
     {
-      name: "bash",
+      name: "Bash - Command line",
+      color: 'lightgreen',
       source:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
     },
   ];
 
+  const changeSkill = (skill) => {
+    setSelectedSkill(skill.name)
+    setSkillColor(skill.color)
+  }
+
   const technologiesMapped = technologies.map((item) => {
-    return <img className='skills-image' src={item.source} alt="skill" />
+    return <img className='skills-image' src={item.source} alt="skill" onClick={() => { changeSkill(item) }} />
   })
 
   return (
     <>
       <div className="skills-container">
-        <h2 className="skills-header">Technologies I work with...</h2>
+        <h2 className="skills-header" style={{color: skillColor}}>{selectedSkill}</h2> 
         <div className="technologies">
           {technologiesMapped}
         </div>
