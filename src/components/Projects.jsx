2178aca53c3ef51fefb6db51e22802e6b7d4b730
rhysez <121772484+rhysez@users.noreply.github.com>
2023-10-07 15:12:37 +0100

    update projectList item properties with new images and sources

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,49 +1,64 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Projects = (props) => {
+  const projectList = [
+    {
+      name: "PokeGuess",
+      description:
+        "Pokemon themed memory card game built with React, uses PokeAPI to asynchronously fetch pokemon data.",
+      image: '/public/poke-guess.png',
+      source: 'https://pokemonguessr.netlify.app/'
+    },
+    {
+      name: "YourDoing",
+      description:
+        "To do list app built using vanilla JavaScript, uses object oriented programming and localStorage API.",
+      image: "/public/todolistdesktopdark.png",
+      source: 'https://rhysez.github.io/todolist/'
+    },
+    {
+      name: "Weather.js",
+      description:
+        "Weather app built that uses asynchronous code to fetch real-time weather information using WeatherAPI.",
+      image: "/public/weather-desktop-example.png",
+      source: 'https://rhysez.github.io/weather-app/'
+    },
+    {
+      name: "eBook Library",
+      description:
+        "Fake eBook library app which uses object oriented programming to generate books.",
+      image: "/public/library-example.png",
+      source: 'https://rhysez.github.io/library/'
+    },
+    {
+      name: "Battleship",
+      description: "Battleship game playable against a simple AI.",
+      image: "/public/battleship-example.png",
+      source: 'https://rhysez.github.io/battleship/'
+    },
+    {
+      name: "Portfolio",
+      description:
+        "This website! It was built using React, and uses Vite as a bundler.",
+      image: '/public/portfolio-home.png',
+    },
+  ];
 
-    const projectList = [
-        {
-            name: 'PokeGuess',
-            description: 'Pokemon themed memory card game built with React, uses PokeAPI to asynchronously fetch pokemon data.'
-        },
-        {
-            name: 'YourDoing',
-            description: 'To do list app built using vanilla JavaScript, uses object oriented programming and localStorage API.',
-            image: '/public/todolistdesktopdark.png'
-        },
-        {
-            name: 'Weather.js',
-            description: 'Weather app built that uses asynchronous code to fetch real-time weather information using WeatherAPI.',
-            image: '/public/weather-desktop-example.png'
-        },
-        {
-            name: 'eBook Library',
-            description: 'Fake eBook library app which uses object oriented programming to generate books.',
-            image: '/public/library-example.png'
-        },
-        {
-            name: 'Battleship',
-            description: 'Battleship game playable against a simple AI.',
-            image: '/public/battleship-example.png'
-        },
-        {
-            name: 'Portfolio',
-            description: 'This website! It was built using React, and uses Vite as a bundler.',
-        }
-    ]
-
-    const projectListMapped = projectList.map((item, index) => {
-        return <div className='project' key={index} style={{backgroundImage: `url(${item.image})`}}></div>
-    })
-
+  const projectListMapped = projectList.map((item, index) => {
     return (
-        <>
-            <div className='projects-container'>
-                {projectListMapped}
-            </div>
-        </>
-    )
-}
+      <div
+        className="project"
+        key={index}
+        style={{ backgroundImage: `url(${item.image})` }}
+      ></div>
+    );
+  });
 
-export default Projects
+  return (
+    <>
+      <div className="projects-container">{projectListMapped}</div>
+    </>
+  );
+};
+
+export default Projects;
