de03aa6a3fc769c7e23d8ad2ef0531d2aed6b624
Rhys <rhyshodgson97@gmail.com>
2024-06-01 16:36:16 +0100

    add initials to nav-bar, change html background color

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,9 +1,16 @@
 import Icon from "@mdi/react";
-import { mdiInstagram, mdiGithub, mdiLinkedin } from "@mdi/js";
+import {mdiInstagram, mdiGithub, mdiLinkedin} from "@mdi/js";
 
 const TopNav = () => {
     return (
         <nav className="nav-bar">
+            <p style={{
+                fontWeight: 'bold',
+                fontSize: '2.25rem',
+                marginLeft: '15%',
+                marginTop: 0,
+                marginBottom: 0
+            }}>Rh<span className={"accent-full-stop"}>.</span></p>
             <div className="socials">
                 <a href="https://www.linkedin.com/in/rhys-hodgson-8262b3218/" target="_blank">
                     <Icon path={mdiLinkedin} size={2} className="nav-social-option"/>
